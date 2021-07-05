0627ff07963a0587ee35e86b4223ac1f24df674b
Brian Hackett <bhackett1024@gmail.com>
2021-07-05 13:29:52 -0600

    Revert WasmEngine mutex ordering to avoid problems during GC

--- src/wasm/wasm-engine.cc
+++ src/wasm/wasm-engine.cc
@@ -425,8 +425,7 @@ struct WasmEngine::NativeModuleInfo {
   int8_t num_code_gcs_triggered = 0;
 };
 
-WasmEngine::WasmEngine()
-  : code_manager_(FLAG_wasm_max_code_space * MB), mutex_("WasmEngine.mutex_") {}
+WasmEngine::WasmEngine() : code_manager_(FLAG_wasm_max_code_space * MB) {}
 
 WasmEngine::~WasmEngine() {
 #ifdef V8_ENABLE_WASM_GDB_REMOTE_DEBUGGING
