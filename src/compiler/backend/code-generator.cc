1917249bb4910d573cb8d2f0900116d5724c9202
Kannan Vijayan <kv-github@kvijayan.com>
2023-01-19 15:03:43 -0500

    Add RecordReplayDisallowEventsWithLabel API.

--- src/compiler/backend/code-generator.cc
+++ src/compiler/backend/code-generator.cc
@@ -468,7 +468,7 @@ base::OwnedVector<byte> CodeGenerator::GetProtectedInstructionsData() {
 }
 
 MaybeHandle<Code> CodeGenerator::FinalizeCode() {
-  recordreplay::AutoDisallowEvents disallow;
+  recordreplay::AutoDisallowEvents disallow("CodeGenerator::FinalizeCode");
 
   if (result_ != kSuccess) {
     tasm()->AbortedCodeGeneration();
