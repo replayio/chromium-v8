eefcd865f00de29cb98f822fae7a6b66e2de972e
Logan Smyth <loganfsmyth@gmail.com>
2023-01-23 12:29:20 -0800

    Indicate clearly when there is no pending exception

--- src/inspector/v8-debugger-agent-impl.h
+++ src/inspector/v8-debugger-agent-impl.h
@@ -146,8 +146,7 @@ class V8DebuggerAgentImpl : public protocol::Debugger::Backend {
   Response getCallFrames(
       std::unique_ptr<protocol::Array<protocol::Debugger::CallFrame>>* out_callFrames) override;
   Response getTopFrameLocation(Maybe<protocol::Debugger::Location>* out_location) override;
-  Response getPendingException(
-      std::unique_ptr<protocol::Runtime::RemoteObject>* out_exception) override;
+  Response getPendingException(Maybe<protocol::Runtime::RemoteObject>* out_exception) override;
 
   bool enabled() const { return m_enabled; }
 
