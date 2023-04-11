6ee960614b05196670766250ced4729942bef6be
Brian Hackett <bhackett1024@gmail.com>
2023-04-11 10:53:07 -0700

    diagnostics for missing IDs

--- src/debug/debug.cc
+++ src/debug/debug.cc
@@ -3906,6 +3906,27 @@ int RecordReplayObjectId(v8::Isolate* v8_isolate, v8::Local<v8::Context> cx,
   }
 
   if (!allow_create) {
+    recordreplay::Print("RecordReplayObjectId MISSING");
+
+    for (const auto& entry : *gRecordReplayObjectIds) {
+      Local<v8::Value> object_ids_val = entry.object_ids_.Get(v8_isolate);
+      Handle<JSWeakMap> object_ids = Handle<JSWeakMap>::cast(Utils::OpenHandle(*object_ids_val));
+
+      Handle<Object> existing(EphemeronHashTable::cast(object_ids->table()).Lookup(object), isolate);
+      if (!existing->IsTheHole(isolate)) {
+        v8::Local<v8::Value> id_value = v8::Utils::ToLocal(existing);
+        if (id_value->IsInt32()) {
+          int id = id_value.As<v8::Int32>()->Value();
+          recordreplay::Print("RecordReplayObjectId FOUND %d", id);
+        } else {
+          recordreplay::Print("RecordReplayObjectId NOT_INTEGER");
+        }
+      } else {
+        recordreplay::Print("RecordReplayObjectId NO_ENTRY");
+      }
+    }
+
+    recordreplay::Print("RecordReplayObjectId MISSING DONE");
     return 0;
   }
 
