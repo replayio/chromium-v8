a59ef52975450504e6c52a5c8a449aea064d5b22
Brian Hackett <bhackett1024@gmail.com>
2023-06-09 05:01:27 -0700

    Feature tweaks for diagnosing load times

--- src/api/api.cc
+++ src/api/api.cc
@@ -11933,33 +11933,35 @@ void recordreplay::SetRecordingOrReplaying(void* handle) {
                            internal::RecordReplayCallbackAssertDescribeData);
   }
 
-  // Set flags to disable non-deterministic posting of tasks to other threads.
-  // We don't support this yet when recording/replaying.
-  internal::FLAG_concurrent_array_buffer_sweeping = false;
-  internal::FLAG_concurrent_marking = false;
-  internal::FLAG_concurrent_sweeping = false;
-  internal::FLAG_incremental_marking_task = false;
-  internal::FLAG_parallel_compaction = false;
-  internal::FLAG_parallel_marking = false;
-  internal::FLAG_parallel_pointer_update = false;
-  internal::FLAG_parallel_scavenge = false;
-  internal::FLAG_scavenge_task = false;
+  if (V8RecordReplayFeatureEnabled("v8-flags", nullptr)) {
+    // Set flags to disable non-deterministic posting of tasks to other threads.
+    // We don't support this yet when recording/replaying.
+    internal::FLAG_concurrent_array_buffer_sweeping = false;
+    internal::FLAG_concurrent_marking = false;
+    internal::FLAG_concurrent_sweeping = false;
+    internal::FLAG_incremental_marking_task = false;
+    internal::FLAG_parallel_compaction = false;
+    internal::FLAG_parallel_marking = false;
+    internal::FLAG_parallel_pointer_update = false;
+    internal::FLAG_parallel_scavenge = false;
+    internal::FLAG_scavenge_task = false;
 
-  // Incremental GC is also disabled for now.
-  internal::FLAG_incremental_marking = false;
+    // Incremental GC is also disabled for now.
+    internal::FLAG_incremental_marking = false;
 
-  // Disable wasm background compilation.
-  internal::FLAG_wasm_num_compilation_tasks = 0;
-  internal::FLAG_wasm_async_compilation = false;
+    // Disable wasm background compilation.
+    internal::FLAG_wasm_num_compilation_tasks = 0;
+    internal::FLAG_wasm_async_compilation = false;
 
-  // The compilation cache can interfere with getting consistent script IDs.
-  internal::FLAG_compilation_cache = false;
+    // The compilation cache can interfere with getting consistent script IDs.
+    internal::FLAG_compilation_cache = false;
 
-  if (V8RecordReplayFeatureEnabled("disable-baseline-jit", nullptr)) {
-    internal::v8_flags.sparkplug = false;
-  }
-  if (!V8RecordReplayFeatureEnabled("use-optimizing-jit", nullptr)) {
-    internal::v8_flags.turbofan = false;
+    if (V8RecordReplayFeatureEnabled("disable-baseline-jit", nullptr)) {
+      internal::v8_flags.sparkplug = false;
+    }
+    if (!V8RecordReplayFeatureEnabled("use-optimizing-jit", nullptr)) {
+      internal::v8_flags.turbofan = false;
+    }
   }
 
   // Write out our pid to a file if specified.
