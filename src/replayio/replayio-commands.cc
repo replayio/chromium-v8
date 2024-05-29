5a9dbb7eabbf5d33bd2e429d851072ce0a47f2bf
D. Seifert <Domiii@users.noreply.github.com>
2024-05-29 13:43:20 +0200

    whoops

--- src/replayio/replayio-commands.cc
+++ src/replayio/replayio-commands.cc
@@ -224,7 +224,7 @@ void RecordReplayRegisterScript(i::Handle<i::Script> script) {
   }
   gRegisteredScripts->insert(script->id());
 
-  if (!recordreplay::RecordReplayIsInternalReplayJs(url)) {
+  if (!replayio::RecordReplayIsInternalReplayJs(url)) {
     // The first "internal Replay JS" script initializes the event emitter.
     // Since this runs beforehand, we have a chicken-and-egg problem.
     // Resolve this by simply not emitting newScript events for internal
