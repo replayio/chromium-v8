619d39072d47db6e153b850c5330ed2eae23ace8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-04-23 18:16:51 +0200

    use ternary

--- src/interpreter/bytecode-generator.cc
+++ src/interpreter/bytecode-generator.cc
@@ -1732,13 +1732,13 @@ void BytecodeGenerator::VisitStatements(
 }
 
 void BytecodeGenerator::ReplayExpressionShiftedSetStatementPosition(Statement* stmt, Expression* expr) {
-  recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: stmt->position(): %d %d\n", script_->id(), stmt->position());
-  recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: expr->position(): %d %d\n", script_->id(), expr->position());
+  recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: stmt->position(): %d %d\n", script_.is_null() ? -1 : script_->id(), stmt->position());
+  recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: expr->position(): %d %d\n", script_.is_null() ? -1 : script_->id(), expr->position());
   builder()->SetStatementPosition(stmt, /* record_replay_breakpoint */ false);
   if (expr->position() < 0) {
     // expression is empty, so the breakpoint gets added at the statement itself
     builder()->RecordReplayInstrumentation("breakpoint", stmt->position());
-    recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: add at stmt position: %d %d\n", script_->id(), stmt->position());
+    recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: add at stmt position: %d %d\n", script_.is_null() ? -1 : script_->id(), stmt->position());
   } else if (expr->IsBinaryOperation()) {
     // given the breakpoint gets shifted to the expression (if that's available) by default
     // we first check if it's not a binary operation, in which case we need to make sure the breakpoint is added at the position of the left operand
@@ -1748,10 +1748,10 @@ void BytecodeGenerator::ReplayExpressionShiftedSetStatementPosition(Statement* s
     // without this specialcase the breakpoints would be added like this (3 would be skipped):
     //
     // return /*2*/foo(), /*1*//*3*/bar();
-    recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: add at left operand: %d %d\n", script_->id(), expr->AsBinaryOperation()->left()->position());
+    recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: add at left operand: %d %d\n", script_.is_null() ? -1 : script_->id(), expr->AsBinaryOperation()->left()->position());
     builder()->RecordReplayInstrumentation("breakpoint", expr->AsBinaryOperation()->left()->position());
   } else {
-    recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: add at expression: %d %d\n", script_->id(), expr->position());
+    recordreplay::Print("ReplayExpressionShiftedSetStatementPosition: add at expression: %d %d\n", script_.is_null() ? -1 : script_->id(), expr->position());
     builder()->RecordReplayInstrumentation("breakpoint", expr->position());
   }
 }
@@ -5680,9 +5680,9 @@ void BytecodeGenerator::VisitCall(Call* expr) {
   //             `ExpressionStatement`'s position.
   //       Example: `/*BREAK1*/o.func/*BREAK2*/();`
 
-  recordreplay::Print("VisitCall: call_head_token_position: %d %d\n", script_->id(), expr->call_head_token_position());
-  recordreplay::Print("VisitCall: start_locations_size: %d %d\n", script_->id(), start_locations_size);
-  recordreplay::Print("VisitCall: builder()->record_replay_instrumentation_site_locations_.size(): %d %d\n", script_->id(), builder()->record_replay_instrumentation_site_locations_.size());
+  recordreplay::Print("VisitCall: call_head_token_position: %d %d\n", script_.is_null() ? -1 : script_->id(), expr->call_head_token_position());
+  recordreplay::Print("VisitCall: start_locations_size: %d %d\n", script_.is_null() ? -1 : script_->id(), start_locations_size);
+  recordreplay::Print("VisitCall: builder()->record_replay_instrumentation_site_locations_.size(): %d %d\n", script_.is_null() ? -1 : script_->id(), builder()->record_replay_instrumentation_site_locations_.size());
 
   if (expr->call_head_token_position() && start_locations_size != builder()->record_replay_instrumentation_site_locations_.size()) {
     // Has arguments and visiting them added breakpoints.
